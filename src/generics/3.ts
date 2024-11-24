9f5d58cb805d1192e481f7d69e12d7c16f1f6cde
stanislav-nedelchev <162455551+stanislav-nedelchev@users.noreply.github.com>
2024-11-24 21:20:40 +0200

    fix mistakes

--- src/generics/3.ts
+++ src/generics/3.ts
@@ -5,7 +5,7 @@
 //   return Object.assign(objA, objB);
 // }
 
-function merge<T, U>(objA: T, objB: U): T & U {
+function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
   return Object.assign({}, objA, objB);
 }
 
